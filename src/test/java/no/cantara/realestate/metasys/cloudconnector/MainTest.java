378e34704616350c0399a59c9655e1eedc54d1bb
baardl <bard.lind@gmail.com>
2023-04-26 10:02:17 +0200

    Initial Commit

---
+++ src/test/java/no/cantara/realestate/metasys/cloudconnector/MainTest.java
@@ -0,0 +1,19 @@
+package no.cantara.realestate.metasys.cloudconnector;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
